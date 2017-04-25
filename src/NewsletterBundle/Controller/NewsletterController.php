583e3aea7dc14ca0a6955a2ddeb398b5e1655384
Anna Krawet <anna.krawet@xsolve.pl>
2017-04-25 16:54:27 +0200

    first version of article class + added nelmio_api_doc

--- src/NewsletterBundle/Controller/NewsletterController.php
+++ src/NewsletterBundle/Controller/NewsletterController.php
@@ -3,16 +3,34 @@
 namespace NewsletterBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use NewsletterBundle\Entity\Article;
 
 class NewsletterController extends FOSRestController
 {
 
+    /**
+     * @ApiDoc(
+     *  description="Returns a collection of Object",
+     *  requirements={
+     *      {
+     *          "name"="article",
+     *          "dataType"="integer",
+     *          "requirement"="\d+",
+     *          "description"="id of article to return"
+     *      }
+     *  }
+     * )
+     * @param Article $article
+     * @return Article
+     */
+
     public function getArticleAction($article)
     {
-        return array('hello' => 'world');
-        //return $article;
+        $article = new Article("test title", "test body");
+        return $article;
     }
+
 
     public function getArticlesAction()
     {
--- src/NewsletterBundle/Entity/Article.php
+++ src/NewsletterBundle/Entity/Article.php
@@ -2,8 +2,40 @@
 
 namespace NewsletterBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ */
 class Article
 {
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $body;
+
+    public function __construct($title = '', $body = '')
+    {
+        //FIXME remove later
+        $this->id   = 1;
+        $this->title = $title;
+        $this->body = $body;
+    }
 }
